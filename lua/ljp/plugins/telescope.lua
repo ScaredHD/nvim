f778e4b9a61e97657b70b8e68bd40bfb0e2353ea
Li Jianping <1196934490@qq.com>
2025-12-09 12:49:04 +0800

    Add telescope-fzf-native.nvim dependency and keymap for config directory search

--- lua/ljp/plugins/telescope.lua
+++ lua/ljp/plugins/telescope.lua
@@ -1,12 +1,16 @@
 return {
   'nvim-telescope/telescope.nvim',
   branch = '0.1.x',
-  dependencies = { 'nvim-lua/plenary.nvim' },
+  dependencies = { 'nvim-lua/plenary.nvim', { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' } },
   config = function()
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
     vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+    vim.keymap.set('n', '<leader>fv', function()
+      builtin.find_files({ cwd = vim.fn.stdpath("config") })
+    end)
+
   end
 }
