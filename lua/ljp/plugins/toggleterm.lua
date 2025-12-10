2877f3cf859d3b2487628331cd0a7c461cdd3ee9
Li Jianping <1196934490@qq.com>
2025-12-10 11:48:08 +0800

    F5 to open toggleterm to compile and run C++ source file

--- lua/ljp/plugins/toggleterm.lua
+++ lua/ljp/plugins/toggleterm.lua
@@ -2,6 +2,17 @@ return {
   'akinsho/toggleterm.nvim', 
   version = "*", 
   opts = {
-    open_mapping = [[<c-\>]]
+    open_mapping = [[<c-\>]],
+    shell = "pwsh.exe"
+  },
+  keys = {
+    { '<F5>', 
+      function() 
+        require('ljp.utils.build-run').build_and_run()
+      end,
+      desc = "Build and run C++ in ToggleTerm"
+    }
   }
+  
+
 }
