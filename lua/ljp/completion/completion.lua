edc04b5fe1fa3900cc5867a7a88108ee4276c607
Li Jianping <1196934490@qq.com>
2026-02-12 16:47:30 +0800

    Setup cmdline mode cmp behavior

--- lua/ljp/completion/completion.lua
+++ lua/ljp/completion/completion.lua
@@ -5,7 +5,6 @@ return {
   { 'hrsh7th/cmp-cmdline' },
   {
     'hrsh7th/nvim-cmp',
-
     config = function()
       local cmp = require 'cmp'
       local ls = require 'luasnip'
@@ -29,17 +28,37 @@ return {
         },
 
         mapping = {
-          ['<Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.confirm({ select = true })
-            elseif copilot.is_visible() then
-              copilot.accept_line()
-            elseif ls.expandable() then
-              ls.expand()
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
+          ['<Tab>'] = cmp.mapping({
+            i = function(fallback)
+              if cmp.visible() then
+                cmp.confirm({ select = true })
+              elseif copilot.is_visible() then
+                copilot.accept_line()
+              elseif ls.expandable() then
+                ls.expand()
+              else
+                fallback()
+              end
+            end,
+            s = function(fallback)
+              if cmp.visible() then
+                cmp.confirm({ select = true })
+              elseif copilot.is_visible() then
+                copilot.accept_line()
+              elseif ls.expandable() then
+                ls.expand()
+              else
+                fallback()
+              end
+            end,
+            c = function()
+              if cmp.visible() then
+                cmp.confirm({ select = true })
+              else
+                cmp.complete()
+              end
+            end,
+          }),
 
           ['<S-Tab>'] = cmp.mapping(function(fallback)
             if copilot.is_visible() then
@@ -47,20 +66,20 @@ return {
             end
           end, { 'i', 's' }),
 
-          ['<A-j>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }, {'i', 's'}),
-          ['<A-k>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }, {'i', 's'}),
+          ['<A-j>'] = cmp.mapping(cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }), { 'i', 's', 'c' }),
+          ['<A-k>'] = cmp.mapping(cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }), { 'i', 's', 'c' }),
 
-          ['<A-n>'] = cmp.mapping(function(fallback) 
+          ['<A-n>'] = cmp.mapping(function(fallback)
             if ls.locally_jumpable(1) then
               ls.jump(1)
             end
-          end, {'i', 's'}),
+          end, { 'i', 's' }),
 
           ['<A-p>'] = cmp.mapping(function(fallback)
             if ls.locally_jumpable(-1) then
               ls.jump(-1)
             end
-          end, {'i', 's'}),
+          end, { 'i', 's' }),
 
           ['<A-u>'] = cmp.mapping.scroll_docs(-4),
           ['<A-d>'] = cmp.mapping.scroll_docs(4),
@@ -98,7 +117,6 @@ return {
       )
 
       cmp.setup.cmdline(':', {
-        mapping = cmp.mapping.preset.cmdline(),
         sources = cmp.config.sources({
           { name = 'path' },
           { name = 'cmdline' }
