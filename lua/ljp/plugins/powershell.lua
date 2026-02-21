51ab13a9803f5348600d85abac48c5bf2504ae04
Li Jianping <1196934490@qq.com>
2026-02-21 15:19:37 +0800

    Clean up plugins

--- lua/ljp/plugins/powershell.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "TheLeoP/powershell.nvim",
-  config = function()
-    require("powershell").setup({
-      bundle_path = vim.fn.stdpath("data") .. "/mason/packages/powershell-editor-services",
-      shell = "powershell"
-    })
-  end
-}
