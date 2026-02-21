dc334e007e3ca6f4e6ab6efda1a47568e3f80a5a
Li Jianping <1196934490@qq.com>
2026-02-21 15:18:44 +0800

    Add snacks

---
+++ lua/ljp/plugins/snacks.lua
@@ -0,0 +1,21 @@
+return {
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  opts = {
+    animate = { enabled = true },
+    bigfile = { enabled = true },
+    dashboard = { enabled = true },
+    indent = {
+      enabled = true,
+      indent = { enabled = false },
+      scope = { enabled = true },
+    },
+    input = { enabled = true },
+    notifier = { enabled = true },
+    quickfile = { enabled = true },
+    scope = { enabled = true },
+    statuscolumn = { enabled = true },
+    words = { enabled = true },
+  },
+}
