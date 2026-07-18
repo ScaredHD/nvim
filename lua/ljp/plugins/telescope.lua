2fe6dd7f5437eb94c5b1c83373f5234e29df2f38
Li Jianping <1196934490@qq.com>
2026-07-18 16:36:48 +0800

    [Telescope] Add prefix 'f'

--- lua/ljp/plugins/telescope.lua
+++ lua/ljp/plugins/telescope.lua
@@ -3,11 +3,11 @@ return {
     version = '*',
     config = function()
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>f', builtin.find_files, { desc = 'Telescope find files' })
-        vim.keymap.set('n', '<leader>g', builtin.live_grep, { desc = 'Telescope live grep' })
-        vim.keymap.set('n', '<leader>b', builtin.buffers, { desc = 'Telescope buffers' })
-        vim.keymap.set('n', '<leader>h', builtin.help_tags, { desc = 'Telescope help tags' })
-        vim.keymap.set('n', '<leader>c', function()
+        vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+        vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+        vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+        vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+        vim.keymap.set('n', '<leader>fc', function()
             builtin.find_files({ cwd = vim.fn.stdpath("config") })
         end)
     end,
