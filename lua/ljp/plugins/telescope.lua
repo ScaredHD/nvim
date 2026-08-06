26a96e3dc227fe5339bb72f83fd6ae866cf55fc2
Li Jianping <1196934490@qq.com>
2026-08-06 18:07:18 +0800

    Configure telescope-fzf-native

--- lua/ljp/plugins/telescope.lua
+++ lua/ljp/plugins/telescope.lua
@@ -2,18 +2,34 @@ return {
     'nvim-telescope/telescope.nvim',
     version = '*',
     config = function()
+        local telescope = require('telescope')
         local builtin = require('telescope.builtin')
+
+        telescope.setup({
+            extensions = {
+                fzf = {
+                    fuzzy = true,
+                    override_generic_sorter = true,
+                    override_file_sorter = true,
+                    case_mode = 'smart_case',
+                },
+            },
+        })
+        telescope.load_extension('fzf')
+
         vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
         vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
         vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
         vim.keymap.set('n', '<leader>fc', function()
             builtin.find_files({ cwd = vim.fn.stdpath("config") })
-        end)
+        end, {desc = 'Telescope config files'})
     end,
     dependencies = {
         'nvim-lua/plenary.nvim',
-        -- optional but recommended
-        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+        { 
+            'nvim-telescope/telescope-fzf-native.nvim',
+            build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release --target install' 
+        }
     }
 }
